7d6e01bf8792a901d3bd96ca402b08de573b072a
rnarula2 <rnarula2@ncsu.edu>
2023-10-27 13:26:49 -0400

    installing and navbar start

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import NavBar from '@/components/NavBar'
+function App() {
+   return (
+      <div className="app bg-gray-20">
+         raghav
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
+import Logo from "@/assets/Logo.png"
+
+
+
+type Props = {}
+
+const   = (props: Props) => {
+    const flexBetween = 'flex items-center justify-between'
+
+    return (
+        <nav>  
+            <div>
+                
+            </div>
+        </nav>
+    )
+}
+
+export default  
